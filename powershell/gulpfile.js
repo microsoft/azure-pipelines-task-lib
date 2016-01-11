7950c3dd271cf6d52fa00e88a97469364b868c65
Bryan MacFarlane <bryanmac@microsoft.com>
2016-01-11 17:58:17 -0500

    build powershell sdk

---
+++ powershell/gulpfile.js
@@ -0,0 +1,57 @@
+var fs = require('fs');
+var path = require('path');
+var gulp = require('gulp');
+var del = require('del');
+var semver = require('semver');
+var spawn = require('child_process').spawn;
+var shell = require('shelljs');
+
+var buildRoot = path.join(__dirname, '_build');
+
+var MIN_NODE_VER = '4.0.0';
+if (semver.lt(process.versions.node, MIN_NODE_VER)) {
+    console.error('requires node >= ' + MIN_NODE_VER + '.  installed: ' + process.versions.node);
+    process.exit(1);
+}
+
+function errorHandler(err) {
+    process.exit(1);
+}
+
+//---------------------------------------------------------------
+// gulp build
+//---------------------------------------------------------------
+gulp.task('clean', function (done) {
+    return del([buildRoot], done);
+});
+
+gulp.task('copy:manifest', ['clean'], function () {
+    return gulp.src(['package.json', '../LICENSE', '../README.md'])
+        .pipe(gulp.dest(buildRoot))
+});
+
+gulp.task('build:lib', ['copy:manifest'], function () {
+    return gulp.src(['VstsTaskSdk/**/*'])
+        .pipe(gulp.dest(path.join(buildRoot, 'VstsTaskSdk')))
+});
+
+
+gulp.task('default', ['build:lib']);
+
+//---------------------------------------------------------------
+// gulp publish (to npm)
+// You need npm user and creds configure on your box to run this
+//---------------------------------------------------------------
+
+gulp.task('prepublish', function (done) {
+    return del([
+        path.join(buildRoot, 'test')],
+        done);
+});
+
+gulp.task('publish', ['prepublish'], function (done) {
+    shell.pushd(buildRoot);
+    spawn('npm', ['publish'], { stdio: 'inherit' }).on('close', done);
+    shell.popd();
+});
+
