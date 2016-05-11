4612c0409a2bf863dbcfdab881c141d640658dff
Eric Sciple <ersciple@microsoft.com>
2016-05-11 03:48:20 -0400

    .

--- powershell/VstsTaskSdk/OutFunctions.ps1
+++ powershell/VstsTaskSdk/OutFunctions.ps1
@@ -60,90 +60,85 @@ function Get-SafeBoundarySubstring {
     [CmdletBinding()]
     param(
         [string]$Str,
-
-        [Parameter(Mandatory = $true, ParameterSetName = 'StartIndex')]
         [int]$StartIndex,
-
-        [Parameter(Mandatory = $true, ParameterSetName = 'EndIndex')]
-        [int]$EndIndex,
-
-        [Parameter(Mandatory = $true)]
         [int]$DesiredLength,
-
-        [Parameter(ParameterSetName = 'StartIndex')]
         [switch]$NoEllipsis)
 
-    if ($Str.Length -eq 0) {
-        return $Str
+    # Short-circuit if at the end of the string.
+    $private:availableLength = $Str.Length - $StartIndex
+    if ($private:availableLength -eq 0) {
+        return ''
     }
 
-    if ($PSCmdlet.ParameterSetName -eq 'StartIndex') {
-        $DesiredLength = [System.Math]::Min(
-            $DesiredLength,
-            $Str.Length - $StartIndex)
-        # Check if the end index is not a whitespace character.
-        $private:endIndex = $StartIndex + $DesiredLength - 1
-        if (![char]::IsWhiteSpace($Str[$private:endIndex])) {
-            # Stop when the end of the string is reached
-            # or when a following whitespace character is reached.
-            while ($private:endIndex + 1 -lt $Str.Length -and
-                ![char]::IsWhiteSpace($Str[$private:endIndex + 1])) {
+    # Adjust the desired length to prevent going beyond the available length.
+    $DesiredLength = [System.Math]::Min(
+        $private:availableLength,
+        $DesiredLength)
 
-                $private:endIndex++
-            }
+    # Check if the end index is not a whitespace character.
+    $private:endIndex = $StartIndex + $DesiredLength - 1
+    if (![char]::IsWhiteSpace($Str[$private:endIndex])) {
+        # Stop when the end of the string is reached
+        # or when a following whitespace character is reached.
+        while ($private:endIndex + 1 -lt $Str.Length -and
+            ![char]::IsWhiteSpace($Str[$private:endIndex + 1])) {
+
+            $private:endIndex++
         }
-
-        $private:substr = $Str.Substring(
-            $StartIndex, # startIndex
-            $private:endIndex - $StartIndex + 1) # length
-        if ($private:endIndex -ne $Str.Length)
     }
+
+    # Extract the substring.
+    $private:substr = $Str.Substring(
+        $StartIndex, # startIndex
+        $private:endIndex - $StartIndex + 1) # length
+
+    # Add ellipsis if desired is less than available.
+    if ($DesiredLength -lt $private:availableLength -and !$NoEllipsis) {
+        $private:substr = "$private:substr ..."
+    }
+
+    return $private:substr
 }
 
-# function Find-SafeEndIndex {
-#     [CmdletBinding()]
-#     param(
-#         [Parameter(Mandatory = $true)]
-#         [string]$Line,
+function Get-SafeBoundaryTail {
+    [CmdletBinding()]
+    param(
+        [string]$Str,
+        [int]$DesiredLength)
 
-#         [Parameter(Mandatory = $true)]
-#         [int]$EndIndex)
+    # Short-circuit if at the end of the string.
+    $private:availableLength = $Str.Length
+    if ($private:availableLength -eq 0) {
+        return ''
+    }
 
-#     # Return the index if it is a whitespace character.
-#     if ([char]::IsWhiteSpace($Line[$EndIndex])) {
-#         return $EndIndex
-#     }
+    # Adjust the desired length to prevent going beyond the available length.
+    $DesiredLength = [System.Math]::Min(
+        $private:availableLength,
+        $DesiredLength)
 
-#     # Stop when the end of the string or a following whitespace character is reached.
-#     while ($EndIndex + 1 -lt $Line.Length -and
-#         ![char]::IsWhiteSpace($Line[$EndIndex + 1])) {
+    # Check if the start index is not a whitespace character.
+    $private:startIndex = $Str.Length - $DesiredLength
+    if (![char]::IsWhiteSpace($Str[$private:startIndex])) {
+        # Stop when the beginning of the string is reached
+        # or when a preceeding whitespace character is reached.
+        while ($private:startIndex - 1 -gt 0 -and
+            ![char]::IsWhiteSpace($Str[$private:startIndex - 1])) {
 
-#         $EndIndex++
-#     }
+            $private:startIndex--
+        }
+    }
 
-#     return $EndIndex
-# }
-# function Find-SafeStartIndex {
-#     [CmdletBinding()]
-#     param(
-#         [string]$Line,
-#         [int]$StartIndex)
+    # Extract the substring.
+    $private:substr = $Str.Substring($private:StartIndex) # startIndex
 
-#     # Determine the prefix for the Line portion.
-#     $private:prefixStartIndex = [System.Math]::Max(0, $_.InvocationInfo.OffsetInLine - 12)
-#     if ($private:prefixStartIndex -gt 0 -and
-#         ![char]::IsWhiteSpace($_.InvocationInfo.Line[$private:prefixStartIndex]) -and
-#         ![char]::IsWhiteSpace($_.InvocationInfo.Line[$private:prefixStartIndex - 1])) {
+    # Add ellipsis if desired is less than available.
+    if ($DesiredLength -lt $private:availableLength) {
+        $private:substr = "... $private:substr"
+    }
 
-#         # Stop when the beginning of the Line or a preceeding whitespace character is reached.
-#         $private:prefixStartIndex--
-#         while ($private:prefixStartIndex -gt 0 -and
-#             ![char]::IsWhiteSpace($_.InvocationInfo.Line[$private:prefixStartIndex - 1])) {
-
-#             $private:prefixStartIndex--
-#         }
-#     }
-# }
+    return $private:substr
+}
 
 function Format-DebugMessage {
     [CmdletBinding()]
@@ -185,93 +180,24 @@ function Format-ErrorRecord {
 
             # Determine the invocation portion of the Line message.
             $private:tildeCount = $private:split[-3].Length - $private:split[-3].IndexOf('~'[0])
-            $private:invocation = Get-SafeBoundarySubstring -Str $_.InvocationInfo.Line -StartIndex $_.InvocationInfo.OffsetInLine -Length $private:tildeCount
+            $private:invocation = Get-SafeBoundarySubstring -Str $_.InvocationInfo.Line -StartIndex $_.InvocationInfo.OffsetInLine -DesiredLength $private:tildeCount -NoEllipsis
 
             # Determine the maximum suffix portion of the Line message.
             $private:suffix = $_.InvocationInfo.Line.Substring(
                 $_.Invocation.OffsetInLine + $private:invocation.Length) # startIndex - It is OK if the startIndex exceeds the actual last index by 1.
 
-            # Trim the suffix if required.
-            $private:maxLineLength = 78
-            $private:maxSuffixLength = [System.Math]::Max(
+            # Trim the suffix.
+            $private:desiredLineLength = 78
+            $private:desiredSuffixLength = [System.Math]::Max(
                 8, # At least 8 characters.
-                $private:maxLineLength - $private:prefix.Length - $private:invocation.Length)
-            $private:maxSuffixLength = [System.Math]::Min(
-                $private:suffix.Length, # Do not go beyond the length of the string.
-                $private:maxSuffixLength)
-            $private:suffix = Get-SafeBoundarySubstring -Str $private:suffix -StartIndex 0 -Length $private:maxSuffixLength
+                $private:desiredLineLength - $private:prefix.Length - $private:invocation.Length)
+            $private:suffix = Get-SafeBoundarySubstring -Str $private:suffix -StartIndex 0 -DesiredLength $private:desiredSuffixLength
 
-            # Trim the prefix if required.
-            $private:maxPrefixLength = [System.Math]::Max(
+            # Trim the prefix.
+            $private:desiredPrefixLength = [System.Math]::Max(
                 12, # At least 12 characters.
-                $private:maxLineLength - $private:invocation.Length - $private:suffix.Length)
-            $private:maxPrefixLength = [System.Math]::Min(
-                $private:prefix.Length, # Do not go beyond the length of the string.
-                $private:maxPrefixLength)
-            $private:prefix = Get-SafeBoundarySubstring -Str $private:prefix -StartIndex 0 -Length $private:maxPrefixLength
-
-
-
-            if ($private:prefix.Length + $private:invocation.Length + $private:suffix.Length -gt $private:maxLength) {
-                Find-SafeEndIndex -Line $private:suffix -EndIndex $private:
-            }
-
-            # while ($private:invocationEndIndex + 1 -lt $_.InvocationInfo.Line.Length -and
-            #     ![char]::IsWhiteSpace($_.InvocationInfo.Line[$private:invocationEndIndex + 1])) {
-
-            #     $private:invocationEndIndex++
-            # }
-
-            # # Customize the Line portion of the PositionMessage in order avoid leaking truncated secrets.
-            # # For each segment of the Line - leading, invocation, trailing - do not stop until the
-            # # beginning/end of the Line or a whitespace character is reached.
-            # $private:lineMessage = New-Object System.Text.StringBuilder
-
-            # # Determine the invocation portion of the Line message. Stop when the end of the Line or a
-            # # following whitespace character is reached.
-            # $private:tildeCount = $private:split[-3].Length - $private:split[-3].IndexOf('~'[0])
-            # $private:invocationEndIndex = $_.InvocationInfo.OffsetInLine + $private:tildeCount
-            # while ($private:invocationEndIndex + 1 -lt $_.InvocationInfo.Line.Length -and
-            #     ![char]::IsWhiteSpace($_.InvocationInfo.Line[$private:invocationEndIndex + 1])) {
-
-            #     $private:invocationEndIndex++
-            # }
-
-            # $private:invocation = $_.InvocationInfo.Line.Substring(
-            #     $_.InvocationInfo.OffsetInLine, # startIndex
-            #     $private:invocationEndIndex - $_.InvocationInfo.OffsetInLine + 1) # length
-
-            # Determine the prefix for the Line portion. Normally it would only contain up to 12 characters.
-            $private:prefixStartIndex = [System.Math]::Max(0, $_.InvocationInfo.OffsetInLine - 12)
-            if ($private:prefixStartIndex -gt 0 -and
-                ![char]::IsWhiteSpace($_.InvocationInfo.Line[$private:prefixStartIndex]) -and
-                ![char]::IsWhiteSpace($_.InvocationInfo.Line[$private:prefixStartIndex - 1])) {
-
-                # Stop when the beginning of the Line or a preceeding whitespace character is reached.
-                $private:prefixStartIndex--
-                while ($private:prefixStartIndex -gt 0 -and
-                    ![char]::IsWhiteSpace($_.InvocationInfo.Line[$private:prefixStartIndex - 1])) {
-
-                    $private:prefixStartIndex--
-                }
-            }
-
-            # Add the prefix to the Line portion of the PositionMessage message.
-            $private:lineMessage = New-Object System.Text.StringBuilder
-            if ($private:prefixStartIndex -gt 0) {
-                $null = $private:lineMessage.Append('... ')
-            }
-
-            $null = $private:lineMessage.Append(
-                $_.InvocationInfo.Line.Substring($private:prefixStartIndex, $_.InvocationInfo.OffsetInLine))
-
-
-            # Get the suffix.
-            $private:suffix = ''
-            if ($private:prefix.Length + $private:invocation.Length -lt 78 -and
-                $private:invocationEndIndex) {
-                #while ($private:suffixEndIndex -lt $)
-            }
+                $private:desiredLineLength - $private:invocation.Length - $private:suffix.Length)
+            $private:prefix = Get-SafeBoundaryTail -Str $private:prefix -StartIndex 0 -DesiredLength $private:desiredPrefixLength
         } catch {
         }
     }
