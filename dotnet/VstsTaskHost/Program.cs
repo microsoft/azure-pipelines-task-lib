298a9b81a059b1e3392f8fe94290e2ffb262b641
Tingluo Huang <huangtingluo@msn.com>
2017-07-24 00:01:13 -0400

    c

---
+++ dotnet/VstsTaskHost/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+namespace VSTS
+{
+    public class TaskHost
+    {
+        private static CancellationTokenSource tokenSource = new CancellationTokenSource();
+
+        public static int Main(string[] args)
+        {
+            try
+            {
+                Console.CancelKeyPress += delegate (object sender, ConsoleCancelEventArgs e)
+                {
+                    e.Cancel = true;
+                    tokenSource.Cancel();
+                };
+
+                ArgUtil.NotNull(args, nameof(args));
+
+                string assemblyQualifiedName = args.FirstOrDefault();
+                ArgUtil.NotNullOrEmpty(assemblyQualifiedName, nameof(assemblyQualifiedName));
+
+                Type type = Type.GetType(assemblyQualifiedName, throwOnError: true);
+                var vstsTask = Activator.CreateInstance(type) as ITask;
+                ArgUtil.NotNull(vstsTask, nameof(vstsTask));
+
+                vstsTask.RunAsync(tokenSource.Token).GetAwaiter().GetResult();
+
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                TaskLib
+                return 1;
+            }
+        }
+    }
+
+    public static class ArgUtil
+    {
+        public static void NotNull(object value, string name)
+        {
+            if (object.ReferenceEquals(value, null))
+            {
+                throw new ArgumentNullException(name);
+            }
+        }
+
+        public static void NotNullOrEmpty(string value, string name)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentNullException(name);
+            }
+        }
+    }
+}
---
+++ dotnet/VstsTaskLib/VstsTaskLib.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VSTS
+{
+    public interface ITask
+    {
+        Task RunAsync(CancellationToken token);
+    }
+
+    public static class TaskLib
+    {
+    }
+}
