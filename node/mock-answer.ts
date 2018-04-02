d3b2d15ed3f74c4b6cc538454b70f11651e661e0
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2018-04-02 15:14:07 -0400

    Missed a couple mocked commands.  Bump patch version (#334)
    * Missed a couple commands.  Catch unmocked commands at compile time.  Bump patch version
    
    * Need to bump again, had un-synced changes upstream
    
    * Strings should be single-quoted
    
    * Bump minor version
    
    * clarify TODO

--- node/mock-answer.ts
+++ node/mock-answer.ts
@@ -1,5 +1,5 @@
-import path = require('path');
-import fs = require('fs');
+import * as path from 'path';
+import * as fs from 'fs';
 
 export interface TaskLibAnswerExecResult {
     code: number,
@@ -15,9 +15,24 @@ export interface TaskLibAnswers {
     find?: { [key: string]: string[] },
     findMatch?: { [key: string]: string[] },
     ls?: { [key: string]: string },
+    osType?: { [key: string]: string },
     rmRF?: { [key: string]: { success: boolean } },
-    which?: { [key: string]: string; },
+    stats?: { [key: string]: any }, // Can't use `fs.Stats` as most existing uses don't mock all required properties
+    which?: { [key: string]: string },
 }
+
+// TODO TypeScript 2.1: replace with `keyof TaskLibAnswers`
+export type MockedCommand = 'checkPath'
+    | 'cwd'
+    | 'exec'
+    | 'exist'
+    | 'find'
+    | 'findMatch'
+    | 'ls'
+    | 'osType'
+    | 'rmRF'
+    | 'stats'
+    | 'which';
 
 export class MockAnswers {
     private _answers: TaskLibAnswers;
@@ -29,14 +44,14 @@ export class MockAnswers {
         this._answers = answers;
     }
 
-    public getResponse(cmd: string, key: string, debug: (message: string) => void): any {
+    public getResponse(cmd: MockedCommand, key: string, debug: (message: string) => void): any {
         debug(`looking up mock answers for ${JSON.stringify(cmd)}, key '${JSON.stringify(key)}'`);
         if (!this._answers) {
             throw new Error('Must initialize');
         }
 
         if (!this._answers[cmd]) {
-            debug(`no mock responses registered for given cmd`);
+            debug(`no mock responses registered for ${JSON.stringify(cmd)}`);
             return null;
         }
 
