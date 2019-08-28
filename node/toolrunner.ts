e5484f9bdf08231749b33c9714225ca48a5d4251
Tommy Petty <jopetty@microsoft.com>
2019-08-28 15:09:45 -0400

    Back out force flag since failing test is flaky

--- node/toolrunner.ts
+++ node/toolrunner.ts
@@ -17,9 +17,6 @@ export interface IExecOptions extends IExecSyncOptions {
 
     /** optional.  defaults to failing on non zero.  ignore will not fail leaving it up to the caller */
     ignoreReturnCode?: boolean;
-
-    /** optional. force flush of stdout. defaults to false */
-    forceFlushStdOut?: boolean;
 }
 
 /**
@@ -806,13 +803,11 @@ export class ToolRunner extends events.EventEmitter {
         // it is possible for the child process to end its last line without a new line.
         // because stdout is buffered, this causes the last line to not get sent to the parent
         // stream. Adding this event forces a flush before the child streams are closed.
-        if (optionsNonNull.forceFlushStdOut) {
-            cp.stdout.on('finish', () => {
-                if (!optionsNonNull.silent) {
-                    optionsNonNull.outStream.write(os.EOL);
-                }
-            });
-        }
+        cp.stdout.on('finish', () => {
+            if (!optionsNonNull.silent) {
+                optionsNonNull.outStream.write(os.EOL);
+            }
+        });
 
         var stdbuffer: string = '';
         cp.stdout.on('data', (data: Buffer) => {
