94455d09030ebd8a9a82630b9a652680e1131bee
sanjays-ms <v-sanjayse@microsoft.com>
2026-09-11 19:41:11 +0530

    Remove explicit node installation in favor agent node (#1211)
    * remove node installation step
    
    * remove node installation step
    
    * comment out node requirement test case
    
    * normalize paths in windows

--- node/test/dirtests.ts
+++ node/test/dirtests.ts
@@ -25,19 +25,17 @@ describe('Dir Operation Tests', function () {
 
     });
 
-    // this test verifies the expected version of node is being used to run the tests.
-    // 5.10.1 is what ships in the 1.x and 2.x agent.
-    it('is expected version', function (done) {
-        this.timeout(1000);
-
-        console.log('node version: ' + process.version);
-        const supportedNodeVersions = ['v16.13.0'];
-        if (supportedNodeVersions.indexOf(process.version) === -1) {
-            assert.fail(`expected node node version to be one of ${supportedNodeVersions.map(o => o).join(', ')}. actual: ` + process.version);
-        }
-
-        done();
-    });
+    // it('is expected version', function (done) {
+    //     this.timeout(1000);
+    //
+    //     console.log('node version: ' + process.version);
+    //     const supportedNodeVersions = ['v16.13.0'];
+    //     if (supportedNodeVersions.indexOf(process.version) === -1) {
+    //         assert.fail(`expected node node version to be one of ${supportedNodeVersions.map(o => o).join(', ')}. actual: ` + process.version);
+    //     }
+    //
+    //     done();
+    // });
 
     // which tests
     it('which() finds file name', function (done) {
@@ -1671,7 +1669,10 @@ describe('Dir Operation Tests', function () {
         testutil.createSymlinkDir(symlinkDirectory, symlinkLevel2Directory);
         assert.equal(fs.readFileSync(path.join(symlinkDirectory, 'real_file')), 'test file content');
         if (os.platform() == 'win32') {
-            assert.equal(fs.readlinkSync(symlinkLevel2Directory), symlinkDirectory + '\\');
+            assert.equal(
+                path.resolve(fs.readlinkSync(symlinkLevel2Directory)),
+                path.resolve(symlinkDirectory)
+            );
         }
         else {
             assert.equal(fs.readlinkSync(symlinkLevel2Directory), symlinkDirectory);
